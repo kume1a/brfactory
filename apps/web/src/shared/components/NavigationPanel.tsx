db230d69ad06677f3a8f1298ab9d42617a8bacbe
kume1a <kumela011@gmail.com>
2024-09-08 20:30:53 +0400

    export navigation panel to separate component, change/fix its UI

---
+++ apps/web/src/shared/components/NavigationPanel.tsx
@@ -0,0 +1,157 @@
+'use client';
+
+import { useState } from 'react';
+import { Dialog, DialogBackdrop, DialogPanel, TransitionChild } from '@headlessui/react';
+import {
+  Bars3Icon as IconBars3,
+  CalendarIcon as IconCalendar,
+  ChartPieIcon as IconChartPie,
+  XMarkIcon as IconXMark,
+} from '@heroicons/react/24/outline';
+import classNames from 'classnames';
+import Image from 'next/image';
+import IconInstagram from '@public/svg/instagram.svg';
+import IconDashboard from '@public/svg/dasboard.svg';
+import { BlankProfileImage } from './BlankProfileImage';
+
+const navigation = [
+  { name: 'Dashboard', href: '#', icon: IconDashboard, current: true },
+  { name: 'IG', href: '#', icon: IconInstagram, current: false },
+  { name: 'Scheduled', href: '#', icon: IconCalendar, current: false },
+  { name: 'Reports', href: '#', icon: IconChartPie, current: false },
+];
+
+export const NavigationPanel = (): JSX.Element => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  return (
+    <>
+      <Dialog open={sidebarOpen} onClose={setSidebarOpen} className="relative z-50 lg:hidden">
+        <DialogBackdrop
+          transition
+          className="fixed inset-0 bg-gray-900/80 transition-opacity duration-300 ease-linear data-[closed]:opacity-0"
+        />
+
+        <div className="fixed inset-0 flex">
+          <DialogPanel
+            transition
+            className="relative mr-16 flex w-full max-w-xs flex-1 transform transition duration-300 ease-in-out data-[closed]:-translate-x-full"
+          >
+            <TransitionChild>
+              <div className="absolute left-full top-0 flex w-16 justify-center pt-5 duration-300 ease-in-out data-[closed]:opacity-0">
+                <button
+                  type="button"
+                  onClick={() => setSidebarOpen(false)}
+                  className="-m-2.5 p-2.5"
+                >
+                  <IconXMark
+                    aria-hidden="true"
+                    className="h-6 w-6 text-gray-200 hover:text-white transition-colors"
+                  />
+                </button>
+              </div>
+            </TransitionChild>
+
+            <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-white px-6 pb-2">
+              <NavigationHeader />
+              <nav className="flex flex-1 flex-col">
+                <ul role="list" className="flex flex-1 flex-col gap-y-7">
+                  <li>
+                    <NavigationItems />
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </DialogPanel>
+        </div>
+      </Dialog>
+
+      {/* Static sidebar for desktop */}
+      <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
+        <div className="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6">
+          <NavigationHeader />
+          <nav className="flex flex-1 flex-col">
+            <ul role="list" className="flex flex-1 flex-col gap-y-7">
+              <li>
+                <NavigationItems />
+              </li>
+              <li className="-mx-6 mt-auto">
+                <a
+                  href="#"
+                  className="flex items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-textPrimary hover:bg-gray-50"
+                >
+                  <BlankProfileImage containerClassName="w-8 h-8" />
+
+                  <span aria-hidden="true">Tom Cook</span>
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+
+      <OpenSidebarButton onClick={() => setSidebarOpen(true)} />
+    </>
+  );
+};
+
+const NavigationItems = (): JSX.Element => {
+  return (
+    <ul role="list" className="-mx-2 space-y-1">
+      {navigation.map(item => (
+        <li key={item.name}>
+          <a
+            href={item.href}
+            className={classNames(
+              item.current
+                ? 'bg-gray-50 text-textPrimary'
+                : 'text-textSecondary hover:bg-gray-50 hover:text-textPrimary',
+              'group flex gap-x-4 rounded-md p-2 text-sm font-semibold leading-6 transition-colors'
+            )}
+          >
+            <item.icon
+              className={classNames(
+                'w-5 h-5 shrink-0',
+                item.current ? 'text-textPrimary' : 'text-gray-400 group-hover:text-textPrimary'
+              )}
+            />
+
+            {item.name}
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const NavigationHeader = (): JSX.Element => {
+  return (
+    <div className="flex h-16 shrink-0 items-center gap-3">
+      <Image
+        alt="Logo"
+        src="/logo/logo_transparentbg.png"
+        className="rounded-full border-2 border-textSecondary h-8 w-auto"
+        width={56}
+        height={56}
+      />
+      <span className="font-semibold text-sm tracking-wide">BrainRot Factory</span>
+    </div>
+  );
+};
+
+const OpenSidebarButton = ({ onClick }: { onClick: VoidFunction }): JSX.Element => {
+  return (
+    <div className="sticky top-0 z-40 flex items-center gap-x-6 bg-white px-4 py-4 shadow-sm sm:px-6 lg:hidden">
+      <button type="button" onClick={onClick} className="-m-2.5 p-2.5 text-gray-700 lg:hidden">
+        <IconBars3
+          aria-hidden="true"
+          className="h-6 w-6 text-gray-800 hover:text-black trnasition-colors"
+        />
+      </button>
+      <div className="flex-1 text-sm font-semibold leading-6 text-gray-900">Dashboard</div>
+      <a href="#">
+        <BlankProfileImage containerClassName="w-8 h-8" />
+      </a>
+    </div>
+  );
+};
