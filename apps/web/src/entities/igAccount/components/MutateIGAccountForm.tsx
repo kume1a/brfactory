8e6da60205f4f5243608b756e18f22940f7c15bd
kume1a <kumela011@gmail.com>
2024-09-12 23:10:18 +0400

    refactor isSubmitting from ig account form

--- apps/web/src/entities/igAccount/components/MutateIGAccountForm.tsx
+++ apps/web/src/entities/igAccount/components/MutateIGAccountForm.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Field, Form, Formik } from 'formik';
+import { Field, Form, Formik, FormikHelpers } from 'formik';
 import { Input } from '../../../shared/components/Input';
 import { FieldErrorMessage } from '../../../shared/components/FieldErrorMessage';
 import { Button } from '../../../shared/components/Button';
@@ -23,7 +23,7 @@ export const MutateIGAccountForm = (): JSX.Element => {
   const router = useRouter();
 
   const formSchema = useMutateIGAccountFormSchema();
-  const { createIGAccount, updateIGAccount, isExecuting } = useMutateIGAccount();
+  const { createIGAccount, updateIGAccount } = useMutateIGAccount();
   const { getById } = useIGAccountRepository();
 
   const [initialFormValues, setInitialFormValues] = useState<FormValues>({
@@ -52,16 +52,23 @@ export const MutateIGAccountForm = (): JSX.Element => {
     });
   }, [query]);
 
-  const onSubmit = async (values: FormValues): Promise<void> => {
+  const onSubmit = async (
+    values: FormValues,
+    { setSubmitting }: FormikHelpers<FormValues>
+  ): Promise<void> => {
     const igAccountId = query.get('igAccountId');
 
-    if (igAccountId) {
-      await updateIGAccount(igAccountId, values);
-    } else {
-      await createIGAccount(values);
-    }
+    setSubmitting(true);
 
-    router.replace(routes.igAccounts);
+    const success = igAccountId
+      ? await updateIGAccount(igAccountId, values)
+      : await createIGAccount(values);
+
+    setSubmitting(false);
+
+    if (success) {
+      router.replace(routes.igAccounts);
+    }
   };
 
   return (
@@ -69,39 +76,41 @@ export const MutateIGAccountForm = (): JSX.Element => {
       initialValues={initialFormValues}
       enableReinitialize
       validationSchema={formSchema}
-      onSubmit={values => onSubmit(values)}
+      onSubmit={onSubmit}
     >
-      <Form noValidate className="max-w-lg flex flex-col gap-y-4 mt-12">
-        <Input
-          name="username"
-          inputWrapClassName="!bg-primaryContainer"
-          placeholder="Username"
-          renderInputElement={defaultProps => <Field {...defaultProps} />}
-        />
-        <FieldErrorMessage name="username" />
+      {({ isSubmitting }) => (
+        <Form noValidate className="max-w-lg flex flex-col gap-y-4 mt-12">
+          <Input
+            name="username"
+            inputWrapClassName="!bg-primaryContainer"
+            placeholder="Username"
+            renderInputElement={defaultProps => <Field {...defaultProps} />}
+          />
+          <FieldErrorMessage name="username" />
 
-        <Input
-          name="email"
-          type="email"
-          inputWrapClassName="!bg-primaryContainer"
-          placeholder="Email address"
-          renderInputElement={defaultProps => <Field {...defaultProps} />}
-        />
-        <FieldErrorMessage name="email" />
+          <Input
+            name="email"
+            type="email"
+            inputWrapClassName="!bg-primaryContainer"
+            placeholder="Email address"
+            renderInputElement={defaultProps => <Field {...defaultProps} />}
+          />
+          <FieldErrorMessage name="email" />
 
-        <Input
-          name="password"
-          placeholder="Password"
-          inputWrapClassName="!bg-primaryContainer"
-          renderInputElement={defaultProps => <Field {...defaultProps} />}
-        />
-        <FieldErrorMessage name="password" />
+          <Input
+            name="password"
+            placeholder="Password"
+            inputWrapClassName="!bg-primaryContainer"
+            renderInputElement={defaultProps => <Field {...defaultProps} />}
+          />
+          <FieldErrorMessage name="password" />
 
-        <Button type="submit" className="w-full flex gap-2 mt-3">
-          Submit
-          {isExecuting ? <CircularProgressIndicator /> : null}
-        </Button>
-      </Form>
+          <Button type="submit" className="w-full flex gap-2 mt-3">
+            Submit
+            {isSubmitting ? <CircularProgressIndicator /> : null}
+          </Button>
+        </Form>
+      )}
     </Formik>
   );
 };
--- apps/web/src/entities/igAccount/hooks/useMutateIGAccount.ts
+++ apps/web/src/entities/igAccount/hooks/useMutateIGAccount.ts
@@ -1,61 +1,58 @@
 import { useAuth } from '@repo/pocketbase-react';
-import { useState } from 'react';
 import { toast } from 'react-toastify';
 import { useIGAccountRepository } from './useIGAccountRepository';
 import { CreateIGAccountInput, UpdateIGAccountInput } from '../igAccount.type';
 
 export const useMutateIGAccount = (): {
-  createIGAccount: (data: Omit<CreateIGAccountInput, 'user'>) => Promise<void>;
-  updateIGAccount: (id: string, data: UpdateIGAccountInput) => Promise<void>;
-  deleteIGAccount: (id: string) => Promise<void>;
-  isExecuting: boolean;
+  createIGAccount: (data: Omit<CreateIGAccountInput, 'user'>) => Promise<boolean>;
+  updateIGAccount: (id: string, data: UpdateIGAccountInput) => Promise<boolean>;
+  deleteIGAccount: (id: string) => Promise<boolean>;
 } => {
   const { user: authUser } = useAuth();
   const { create, updateById, deleteById } = useIGAccountRepository();
 
-  const [isExecuting, setIsExecuting] = useState(false);
+  return {
+    createIGAccount: async (data: Omit<CreateIGAccountInput, 'user'>) => {
+      try {
+        await create({ ...data, user: authUser?.id });
 
-  const createIGAccount = async (data: Omit<CreateIGAccountInput, 'user'>) => {
-    try {
-      setIsExecuting(true);
-      await create({ ...data, user: authUser?.id });
+        toast.success('IG Account created successfully');
 
-      toast.success('IG Account created successfully');
-    } catch (e) {
-      toast.error('Error creating IG Account');
-      console.error(e);
-    } finally {
-      setIsExecuting(false);
-    }
+        return true;
+      } catch (e) {
+        toast.error('Error creating IG Account');
+        console.error(e);
+      }
+
+      return false;
+    },
+    updateIGAccount: async (id: string, data: UpdateIGAccountInput) => {
+      try {
+        await updateById(id, data);
+
+        toast.success('IG Account updated successfully');
+
+        return true;
+      } catch (e) {
+        toast.error('Error updating IG Account');
+        console.error(e);
+      }
+
+      return false;
+    },
+    deleteIGAccount: async (id: string) => {
+      try {
+        await deleteById(id);
+
+        toast.success('IG Account deleted successfully');
+
+        return true;
+      } catch (e) {
+        toast.error('Error deleting IG Account');
+        console.error(e);
+      }
+
+      return false;
+    },
   };
-
-  const updateIGAccount = async (id: string, data: UpdateIGAccountInput) => {
-    try {
-      setIsExecuting(true);
-      await updateById(id, data);
-
-      toast.success('IG Account updated successfully');
-    } catch (e) {
-      toast.error('Error updating IG Account');
-      console.error(e);
-    } finally {
-      setIsExecuting(false);
-    }
-  };
-
-  const deleteIGAccount = async (id: string) => {
-    try {
-      setIsExecuting(true);
-      await deleteById(id);
-
-      toast.success('IG Account deleted successfully');
-    } catch (e) {
-      toast.error('Error deleting IG Account');
-      console.error(e);
-    } finally {
-      setIsExecuting(false);
-    }
-  };
-
-  return { createIGAccount, updateIGAccount, deleteIGAccount, isExecuting };
 };
