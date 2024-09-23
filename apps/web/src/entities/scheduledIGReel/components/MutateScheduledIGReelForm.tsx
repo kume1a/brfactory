758743340d3982d746837c0e46167d0a6c0cb731
kume1a <kumela011@gmail.com>
2024-09-23 18:06:54 +0400

    make caption text area

--- apps/web/src/entities/scheduledIGReel/components/MutateScheduledIGReelForm.tsx
+++ apps/web/src/entities/scheduledIGReel/components/MutateScheduledIGReelForm.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Field, Form, Formik, FormikHelpers } from 'formik';
+import { Field, FieldAttributes, Form, Formik, FormikHelpers } from 'formik';
 import { Input } from '../../../shared/components/Input';
 import { FieldErrorMessage } from '../../../shared/components/FieldErrorMessage';
 import { Button } from '../../../shared/components/Button';
@@ -88,31 +88,8 @@ export const MutateScheduledIGReelForm = (): JSX.Element => {
     values: FormValues,
     { setSubmitting }: FormikHelpers<FormValues>
   ): Promise<void> => {
-    if (!thumbnailFile || !videoFile) {
-      console.warn('Thumbnail and video are required');
-      return;
-    }
-
-    const scheduledIGReelId = query.get('scheduledIGReelId');
-
-    const input = {
-      ...values,
-      startAt: new Date(values.startAt).toUTCString(),
-      thumbnailFileId: thumbnailFile,
-      videoFileId: videoFile,
-    };
-
-    setSubmitting(true);
-
-    const success = scheduledIGReelId
-      ? await updateScheduledIGReel(scheduledIGReelId, input)
-      : await createScheduledIGReel(input);
-
-    setSubmitting(false);
-
-    if (success) {
-      router.replace(routes.scheduledIGReels);
-    }
+    console.log('values', values);
+    return;
   };
 
   return (
@@ -158,7 +135,17 @@ export const MutateScheduledIGReelForm = (): JSX.Element => {
             name="caption"
             placeholder="Caption"
             inputWrapClassName="!bg-primaryContainer mt-4"
-            renderInputElement={defaultProps => <Field {...defaultProps} />}
+            renderInputElement={defaultProps => (
+              <Field {...defaultProps}>
+                {({ field }: FieldAttributes<any>) => (
+                  <textarea
+                    {...field}
+                    placeholder="Caption"
+                    className="w-full h-32 border-none bg-transparent !ring-0 px-2 leading-5 text-sm py-1.5"
+                  />
+                )}
+              </Field>
+            )}
           />
           <FieldErrorMessage name="caption" />
 
